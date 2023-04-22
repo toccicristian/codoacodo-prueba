3ee4f7b7858bc31f1ea7ec9443bed47054c02c6c
Cristian <toccicristian@hotmail.com>
2023-04-21 21:36:43 -0300

    primera clase tema js

---
+++ practicas/JS-com-23026/js/ejemplo1.js
@@ -0,0 +1,42 @@
+var num=29
+var num2="29"
+
+console.log("console.log(num==num2)")
+console.log(num==num2);
+console.log("console.log(num===num2)")
+console.log(num===num2);
+console.log("console.log(num<30 && num2>3);");
+console.log(num<30 && num2>3);
+console.log("console.log(num>29 || num);");
+console.log(num>29 || num);
+console.log("console.log(num<30 && num2>3);");
+console.log(num<30 && num2>3);
+console.log("console.log(num>29 && num2>3);");
+console.log(num>29 && num2>3);
+console.log("console.log(!(num<30 && num2>3));");
+console.log(!(num<30 && num2>3));
+console.log("console.log(num!=num2);")
+console.log(num!=num2);
+console.log("console.log(num!==num2);")
+console.log(num!==num2);
+
+console.log("var valor4=20;");
+var valor4=20;
+console.log("console.log(valor4++);");
+console.log(valor4++);//20
+console.log("console.log(valor4);");
+console.log(valor4);//21
+console.log("console.log(++valor4);");
+console.log(++valor4);//22
+console.log("console.log(valor4);");
+console.log(valor4);//22
+console.log("----------");
+console.log("console.log(--valor4);");
+console.log(--valor4);//21
+console.log("console.log(valor4);");
+console.log(valor4);//21
+console.log("console.log(valor4--);");
+console.log(valor4--);//21
+console.log("console.log(valor4);");
+console.log(valor4);//20
+
